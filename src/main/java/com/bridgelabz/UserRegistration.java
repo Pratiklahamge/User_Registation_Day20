83b53a1add40ff80c92b18d2b146817e86132e99
Pratiklahamge <plahamge2@gmail.com>
2021-12-23 15:15:32 +0530

    Added:Program for Passward for Rule 4 is in format or not using Test

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -7,68 +7,87 @@ public class UserRegistration {
 	/*
 	 * method to check first name
 	 */
-	
 	public boolean firstName(String firstName) {
-		
+
 		String regex="^[A-z]{1}[a-z]*";
 		Pattern pattern=Pattern.compile(regex);
 		Matcher match=pattern.matcher(firstName);
-		
+
 		return match.matches();
-		
+
 	}
-	
+
 	/*
 	 * method to chek last name 
 	 */
-	
+
 	public boolean lastName(String lastName) {
 		String regex = "^[A-Z]{1}[a-z]{4}";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
-	
 	/*
 	 * method to check email address
 	 */
-	  public boolean emailAddress(String email) {
-	        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(email);
-	        return matcher.matches();
-	    }
-	  /*
-	   * method to validate mobile number
-	   *
-	   */
-	   public boolean phoneNumber(String phoneNumber) {
-	        String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(phoneNumber);
-	        return matcher.matches();
-	    }
-	   /*
-		 * Method to check rule one minimum 8
-		 *Characters - NOTE – All rules must
-		 */
-		public boolean passwordRule1(String password) {
-			String regex = "^[0-9a-zA-Z]{8,}$";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(password);
-			return matcher.matches();
-		}
-		/*
-		 * Method to check rule one minimum 8
-		 *Should have at least 1
-		 *Upper Case - NOTE – All rules must
-		 */
-		public boolean passwordRule2(String password) {
-			String regex = "^[A-Z]{1}[a-z]{8,}$";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(password);
-			return matcher.matches();
+	public boolean emailAddress(String email) {
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	/*
+	 * method to validate mobile number
+	 *
+	 */
+	public boolean phoneNumber(String phoneNumber) {
+		String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(phoneNumber);
+		return matcher.matches();
+	}
+	/*
+	 * Method to check rule one minimum 8
+	 *Characters - NOTE – All rules must
+	 */
+	public boolean passwordRule1(String password) {
+		String regex = "^[0-9a-zA-Z]{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+	}
+	/*
+	 * Method to check rule one minimum 8
+	 *Should have at least 1
+	 *Upper Case - NOTE – All rules must
+	 */
+	public boolean passwordRule2(String password) {
+		String regex = "^[A-Z]{1}[a-z]{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
 
-		}
+	}
+
+	/*
+	 * Should have at least 1 numeric number in the password 
+	 */
+	public boolean passwordRule3(String password) {
+		String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9]).{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+
+	}
+
+	/*
+	 * Has exactly 1 Special Character
+	 */	
+	  public boolean passwordRule4(String password) {
+	        String regex = "^[A-Z]{1}[0-9a-zA-Z!,@#$&*().]{8,}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(password);
+	        return matcher.matches();
+	    }
 
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -9,35 +9,33 @@ public class UserRegistrationTest {
 
 	@Test
 	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-		boolean result = userRegistration.firstName("Pratik");
+		boolean result = userRegistration.firstName("Ashish");
 		Assert.assertEquals(true, result);
 	}
 
 	@Test
 	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-		boolean result = userRegistration.firstName("pratik");
+		boolean result = userRegistration.firstName("ashish");
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
 	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
-		boolean result = userRegistration.lastName("Gund");
+		boolean result = userRegistration.lastName("Surja");
 		Assert.assertEquals(true, result);
 	}
 
 	@Test
 	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
-		boolean result = userRegistration.lastName("gund");
+		boolean result = userRegistration.lastName("surja");
 		Assert.assertEquals(false, result);
-
 	}
-
 	/*
 	 * methods to check mail addresss
 	 */
 	@Test
 	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
-		boolean result = userRegistration.emailAddress("abc@gmail.com");
+		boolean result = userRegistration.emailAddress("abc@gmail.com.com");
 		Assert.assertEquals(true, result);
 	}
 
@@ -46,6 +44,7 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
 		Assert.assertEquals(false, result);
 	}
+
 	/*
 	 * test cases for mobile number validation
 	 */
@@ -59,7 +58,6 @@ public class UserRegistrationTest {
 	public void givenPhoneNumber_WhenOurOfFormat_ShouldReturnFlase() {
 		boolean result = userRegistration.phoneNumber("+91 9604315270");
 		Assert.assertEquals(false, result);
-
 	}
 	/*
 	 * method to validate password
@@ -85,6 +83,28 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.passwordRule2("abcdefghij");
 		Assert.assertEquals(false, result);
 	}
+	  @Test
+	    public void givenPasswordRule3_WhenInFormat_ShouldReturnTrue() {
+	        boolean result = userRegistration.passwordRule3("Rajendra22");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenPasswordRule3_WhenOurOfFormat_ShouldReturnFalse() {
+	        boolean result = userRegistration.passwordRule3("Rajendra");
+	        Assert.assertEquals(false, result);
+	    }
+	    @Test
+	    public void givenPasswordRule4_WhenInFormat_ShouldReturnTrue() {
+	        boolean result = userRegistration.passwordRule4("Rajendra@22");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenPasswordRule4_WhenOurOfFormat_ShouldReturnFalse() {
+	        boolean result = userRegistration.passwordRule4("Rajendra");
+	        Assert.assertEquals(false, result);
+	    }
 	
 }
 
