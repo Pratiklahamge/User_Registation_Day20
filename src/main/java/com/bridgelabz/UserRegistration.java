8ee9047afe44c76c4353b303207fe837a15c41b3
Pratiklahamge <plahamge2@gmail.com>
2021-12-23 14:24:49 +0530

    Added:Program for the Last name name is in format or not using Test Case

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -3,6 +3,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	
+	/*
+	 * method to check first name
+	 */
+	
 	public boolean firstName(String firstName) {
 		
 		String regex="^[A-z]{1}[a-z]*";
@@ -11,6 +16,17 @@ public class UserRegistration {
 		
 		return match.matches();
 		
+	}
+	
+	/*
+	 * method to chek last name 
+	 */
+	
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{4}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
 	}
 
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,15 +7,28 @@ public class UserRegistrationTest {
 	 UserRegistration userRegistration = new UserRegistration();
 
 
-	    @Test
+	 @Test
 	    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Ashish");
+	        boolean result = userRegistration.firstName("Pratik");
 	        Assert.assertEquals(true, result);
 	    }
 
 	    @Test
 	    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-	        boolean result = userRegistration.firstName("ashish");
+	        boolean result = userRegistration.firstName("pratik");
 	        Assert.assertEquals(false, result);
+	    }
+
+	    @Test
+	    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+	        boolean result = userRegistration.lastName("Gund");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+	        boolean result = userRegistration.lastName("gund");
+	        Assert.assertEquals(false, result);
+
 	    }
 }
