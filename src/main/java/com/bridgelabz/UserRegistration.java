fe2599563cd8ec4881bd48b2bac117ff4670e055
Pratiklahamge <plahamge2@gmail.com>
2021-12-23 14:46:19 +0530

    Added:Program for the Phone number is in format or not Using Test Case

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -38,5 +38,15 @@ public class UserRegistration {
 	        Matcher matcher = pattern.matcher(email);
 	        return matcher.matches();
 	    }
+	  /*
+	   * method to validate mobile number
+	   *
+	   */
+	   public boolean phoneNumber(String phoneNumber) {
+	        String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(phoneNumber);
+	        return matcher.matches();
+	    }
 
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -4,46 +4,62 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
-	 UserRegistration userRegistration = new UserRegistration();
+	UserRegistration userRegistration = new UserRegistration();
 
 
-	 @Test
-	    public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Pratik");
-	        Assert.assertEquals(true, result);
-	    }
+	@Test
+	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Pratik");
+		Assert.assertEquals(true, result);
+	}
 
-	    @Test
-	    public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
-	        boolean result = userRegistration.firstName("pratik");
-	        Assert.assertEquals(false, result);
-	    }
+	@Test
+	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("pratik");
+		Assert.assertEquals(false, result);
+	}
 
-	    @Test
-	    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
-	        boolean result = userRegistration.lastName("Gund");
-	        Assert.assertEquals(true, result);
-	    }
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Gund");
+		Assert.assertEquals(true, result);
+	}
 
-	    @Test
-	    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
-	        boolean result = userRegistration.lastName("gund");
-	        Assert.assertEquals(false, result);
+	@Test
+	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("gund");
+		Assert.assertEquals(false, result);
 
-	    }
-	    
-	    /*
-		 * methods to check mail addresss
-		 */
-		@Test
-		public void givenEmail_WhenInFormat_ShouldReturnTrue() {
-			boolean result = userRegistration.emailAddress("abc@gmail.com");
-			Assert.assertEquals(true, result);
-		}
+	}
 
-		@Test
-		public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
-			boolean result = userRegistration.emailAddress("abc()*@gmail.com");
-			Assert.assertEquals(false, result);
-		}
+	/*
+	 * methods to check mail addresss
+	 */
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("abc@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
+	/*
+	 * test cases for mobile number validation
+	 */
+	@Test
+	public void givenPhoneNumber_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.phoneNumber("91 9604315270");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.phoneNumber("+91 9604315270");
+		Assert.assertEquals(false, result);
+
+	}
 }
+
